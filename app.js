649dffb0b74cb3f0f85836465d5c83ef35155645
chandu <chandugnanadeep@gmail.com>
2019-11-19 22:36:29 -0700

    added basic express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // https://github.com/gnanadeep/basic-node-server
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({
+        key: "welcome"
+    });
+});
+
+app.listen(8080);
