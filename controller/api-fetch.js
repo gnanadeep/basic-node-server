2b2e83c3f5c3f50afb31f59f3ea9eec9d3c849d4
chandu <gnanadeep4@gmail.com>
2020-06-16 22:49:20 -0700

    feat(commit): added editor config file

--- controller/api-fetch.js
+++ controller/api-fetch.js
@@ -2,14 +2,14 @@ const fetch = require('node-fetch');
 
 const nodeFetchTest = (request, response) => {
   fetch('https://httpbin.org/get')
-      .then((res) => res.json())
-      .then((json) => {
-        response.send(json);
-      })
-      .catch((error) => {
-        console.log(error);
-        response.status(error.statusCode).send({message: error.statusMessage});
-      });
+    .then((res) => res.json())
+    .then((json) => {
+      response.send(json);
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(error.statusCode).send({message: error.statusMessage});
+    });
 };
 
 const nodeFetchTwoCall = async (request, response) => {
@@ -17,12 +17,12 @@ const nodeFetchTwoCall = async (request, response) => {
     const api1 = await fetch('https://api.github.com/users/github');
     const api2 = await fetch('https://httpbin.org/get');
     Promise.all([api1.json(), api2.json()])
-        .then((results) => {
-          response.send({
-            api1: results[0],
-            api2: results[1],
-          });
+      .then((results) => {
+        response.send({
+          api1: results[0],
+          api2: results[1],
         });
+      });
   } catch (e) {
     console.log(e);
     response.status(e.statusCode).send({message: e.statusMessage});
--- index.js
+++ index.js
@@ -6,30 +6,25 @@ const port = 1111;
 const app = express();
 
 
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log('hello world');
 });
 
 
-app.get('/hello', (req,res) => {
+app.get('/hello', (req, res) => {
   console.log('hello world');
   let a = '';
 
-https.get('https://jsonplaceholder.typicode.com/todos', (response)=> {
-  response.on('data',(chunk)=>{
-    a += chunk;
-  });
+  https.get('https://jsonplaceholder.typicode.com/todos', (response) => {
+    response.on('data', (chunk) => {
+      a += chunk;
+    });
 
-  response.on('end' , () => {
-    // console.log(a);
-    res.send(a);
+    response.on('end', () => {
+      // console.log(a);
+      res.send(a);
+    });
+  }).on('error', (err) => {
+    console.log('Error: ' + err.message);
   });
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
 });
-
-
-
-
-
-})
